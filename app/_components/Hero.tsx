380e50e242eb433047426d0de6c537fbbc07e848
cerebulbshivam <shivmkumar.jayswal@cerebulb.com>
2025-08-24 10:15:20 +0530

    small fix

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -70,7 +70,7 @@ const Hero = () => {
             <Button size="lg">Explore Now</Button>
           </Link>
           <Link href="https://github.com/shivamrj1035" target="_blank">
-            <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
+            <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900 cursor-pointer">
               Contact Support
             </button>
           </Link>
