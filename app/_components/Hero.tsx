fdbf7e830c99d954d401fe52446bb7e862e8f9f7
cerebulbshivam <shivmkumar.jayswal@cerebulb.com>
2025-08-24 10:21:33 +0530

    small fix

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -67,7 +67,9 @@ const Hero = () => {
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
         >
           <Link href="/dashboard">
-            <Button size="lg">Explore Now</Button>
+            <Button size="lg" className="cursor-pointer">
+              Explore Now
+            </Button>
           </Link>
           <Link href="https://github.com/shivamrj1035" target="_blank">
             <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900 cursor-pointer">
